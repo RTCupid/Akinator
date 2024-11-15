033da7dc911e79bc1761b197b4f059060ecbff11
RTCupid <19apr2006@vk.com>
2024-11-15 13:39:32 +0300

    delete unexsist func from h file

--- AkinatorBase.h
+++ AkinatorBase.h
@@ -4,6 +4,4 @@
     errTr_t MakeAkinatorBase (tree_t* akntr, const char* namefile);
 
     node_t* RunAkinatorBase (tree_t* akntr, FILE* base_file);
-
-    void NewNodeInBranch (tree_t* akntr, node_t* crnt_node, brnch_side_t branch_side);
 #endif
